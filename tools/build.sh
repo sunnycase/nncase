cbf93ee0a6e624a5f15d9ebcee36ad1350c909d1
sunnycase <sunnycase@live.cn>
2023-05-29 17:26:23 +0800

    Fix workflow

--- tools/build.sh
+++ tools/build.sh
@@ -4,8 +4,6 @@ export PATH=/opt/python/cp37-cp37m/bin:$PATH
 export BUILD_TYPE=Release
 
 pip install conan==1.58 ninja
-export CC=/opt/rh/devtoolset-10/root/usr/bin/cc
-export CXX=/opt/rh/devtoolset-10/root/usr/bin/c++
 cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=$BUILD_TYPE -BUILD_PYTHON_BINDING=OFF
 cmake --build build --config $BUILD_TYPE
 cmake --install build --prefix install
