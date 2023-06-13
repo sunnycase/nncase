8d01ee3fe911afe8a710c2c6b8060f5e0e8541ff
sunnycase <sunnycase@live.cn>
2023-06-13 19:19:51 +0800

    Fix workflow

--- tools/build-runtime.sh
+++ tools/build-runtime.sh
@@ -4,7 +4,7 @@ export PATH=/opt/python/cp37-cp37m/bin:$PATH
 export BUILD_TYPE=Release
 
 pip install conan==1.58 ninja
-cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=$BUILD_TYPE -BUILD_PYTHON_BINDING=OFF
+cmake -B build -G Ninja -DCMAKE_BUILD_TYPE=$BUILD_TYPE -BUILD_PYTHON_BINDING=OFF -DBUILDING_RUNTIME=ON -DBUILD_TESTING=ON
 cmake --build build --config $BUILD_TYPE
 cmake --install build --prefix install
 conan user sunnycase -r sunnycase -p $1
