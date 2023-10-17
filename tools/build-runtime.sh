7115a7afd4f3c741ece3d21b6e93ea3c55de3d75
sunnycase <sunnycase@live.cn>
2023-10-17 18:45:53 +0800

    Update

--- tools/build-runtime.sh
+++ tools/build-runtime.sh
@@ -4,6 +4,8 @@ export PATH=/opt/python/cp37-cp37m/bin:$PATH
 export BUILD_TYPE=Release
 
 pip install conan==1.58 ninja
-conan install . -if build --build=missing -s build_type=$BUILD_TYPE --profile=default -o runtime=True -o python=False -o tests=True -s compiler.cpp
+conan profile new default --detect
+conan profile update settings.compiler.libcxx=libstdc++11 default
+conan install . -if build --build=missing -s build_type=$BUILD_TYPE --profile=default -o runtime=True -o python=False -o tests=True -s compiler.cppstd=17
 conan user sunnycase -r sunnycase -p $1
 conan upload "*" --all -r sunnycase -c
--- tools/build.sh
+++ tools/build.sh
@@ -4,6 +4,8 @@ export PATH=/opt/python/cp37-cp37m/bin:$PATH
 export BUILD_TYPE=Release
 
 pip install conan==1.58 ninja
-conan install . -if build --build=missing -s build_type=$BUILD_TYPE --profile=default -o runtime=False -o python=False -o tests=True
+conan profile new default --detect
+conan profile update settings.compiler.libcxx=libstdc++11 default
+conan install . -if build --build=missing -s build_type=$BUILD_TYPE --profile=default -o runtime=False -o python=False -o tests=True -s compiler.cppstd=20
 conan user sunnycase -r sunnycase -p $1
 conan upload "*" --all -r sunnycase -c
