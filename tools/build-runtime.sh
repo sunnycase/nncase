8357d43113ca7d10cf4e1a7fcec71cb652a53552
sunnycase <sunnycase@live.cn>
2023-10-17 18:49:26 +0800

    Update

--- tools/build-runtime.sh
+++ tools/build-runtime.sh
@@ -4,6 +4,7 @@ export PATH=/opt/python/cp37-cp37m/bin:$PATH
 export BUILD_TYPE=Release
 
 pip install conan==1.58 ninja
+conan remote add -i 0 sunnycase https://conan.sunnycase.moe
 conan profile new default --detect
 conan profile update settings.compiler.libcxx=libstdc++11 default
 conan install . -if build --build=missing -s build_type=$BUILD_TYPE --profile=default -o runtime=True -o python=False -o tests=True -s compiler.cppstd=17
