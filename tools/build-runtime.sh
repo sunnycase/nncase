ccdffbbe8e65820cea1f6478e07702148dd35638
sunnycase <sunnycase@live.cn>
2024-08-27 09:35:27 +0800

    Use manylinux_2_28_x86_64:1.0

--- tools/build-runtime.sh
+++ tools/build-runtime.sh
@@ -6,5 +6,5 @@ export BUILD_TYPE=Release
 pip install conan==2.6.0
 conan remote add sunnycase https://conan.sunnycase.moe --index 0
 conan install . --build=missing -s build_type=$BUILD_TYPE -pr:a=toolchains/x86_64-linux.profile.jinja -o "&:runtime=True" -o "&:python=True" -o "&:tests=True"
-conan login -p $1 sunnycase sunnycase
+conan remote login -p $1 sunnycase sunnycase
 conan upload "*" -r sunnycase -c
